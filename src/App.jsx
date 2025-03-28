97dba8a865ef1b84418c5e64b837e8ec064d8077
dIznBAN <diznban@gmail.com>
2025-03-28 09:57:50 -0300

    components

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,26 @@ function App() {
 
   // var nome = "São Hugo"
 
+  const Formulario = () => {
+    return (
+      <div>
+        <input className="nome"
+          name="nome"
+          onChange={(e) => { setNome(e.target.value) }}
+          placeholder="Digite um nome..."
+          type="" />
+        <button className="botao"
+          onClick={() => { alert(nome) }}>CLIQUE AQUI</button>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h3>Pizzaria 2F</h3>
-      <input className="nome"
-        name="nome"
-        onChange={(e) => { setNome(e.target.value) }}
-        placeholder="Digite um nome..."
-        type="" />
-      <button className="botao" onClick={() => { alert(nome) }}>CLIQUE AQUI</button>
+    <Formulario />
+    <Formulario />
+    <Formulario />
     </div>
   )
 }
